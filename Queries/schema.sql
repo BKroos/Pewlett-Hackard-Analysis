c3bd31e90ec69d9cc321ba5b848800150f1f40c7
BKroos <btkroos@gmail.com>
2021-06-03 16:19:00 -0500

    Added query and output file for retirement-eligible employees

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -35,7 +35,7 @@ CREATE TABLE salaries (
 );
 
 Create Table managers (
-	dept_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
@@ -44,7 +44,7 @@ Create Table managers (
 	
 Create Table dept_emp (
 	emp_no INT NOT NULL,
-	dept_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
@@ -58,3 +58,4 @@ Create Table titles (
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no));
 
+Select * from titles
